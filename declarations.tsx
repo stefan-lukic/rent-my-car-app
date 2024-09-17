8960603850849338189c720085a24e4731264552
StefanLukic <stefan.lukic@yahoo.com>
2024-09-17 13:46:56 +0200

    fix

--- declarations.tsx
+++ declarations.tsx
@@ -1,7 +1,7 @@
 declare module 'bcryptjs' {
   const bcrypt: {
     hash: (password: string, saltRounds: number) => string;
-    compareSync: (password: string, hash: string) => boolean;
+    compare: (password: string, hash: string) => boolean;
     genSaltSync: (saltRounds?: number) => string;
   };
   export default bcrypt;
