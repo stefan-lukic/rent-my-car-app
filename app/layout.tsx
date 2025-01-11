4b149ce6f3cb1493078099406a6b8c99c6c41b51
StefanLukic <stefan.lukic@yahoo.com>
2025-01-11 17:35:20 +0100

    speed insights try 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
-
 export const metadata: Metadata = {
   title: 'Rent My Car',
   description: 'Car rental service',
