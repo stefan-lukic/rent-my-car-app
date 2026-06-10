fde80854295359b7b7b7cfc76732851ef5468106
cpku21 <95412028+cpku21@users.noreply.github.com>
2026-06-10 18:18:28 +0200

    remove useeffect and states

--- app/cars/add-car/page.tsx
+++ app/cars/add-car/page.tsx
@@ -2,19 +2,13 @@
 
 import MobileAddCar from '@/components/mobile/MobileAddCar';
 import AddCar from '@/components/AddCar';
-import { useState, useEffect } from 'react';
+import { isMobileCSR } from '@/utils/deviceDetectionCSR';
 
 export default function AddCarPage() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    setIsMobile(window.innerWidth <= 680);
-  }, []);
-
+  const isMobile = isMobileCSR();
   return (
     <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white md:pt-24 pb-12 relative">
       {isMobile ? <MobileAddCar /> : <AddCar />}
     </main>
   );
 }
-//ovo sam ubacio jer je server i browser su renderovali razlicite komponete pa je next pucao tj. izbacivao je hydration error
