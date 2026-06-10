7585aa0ec44d95d073d5c906f584eb24ba6a3a49
cpku21 <95412028+cpku21@users.noreply.github.com>
2026-06-10 14:20:55 +0200

    fixed: addcar mobile padding, dropdown arrows, hydration error

--- app/cars/add-car/page.tsx
+++ app/cars/add-car/page.tsx
@@ -2,14 +2,19 @@
 
 import MobileAddCar from '@/components/mobile/MobileAddCar';
 import AddCar from '@/components/AddCar';
-import { isMobileCSR } from '@/utils/deviceDetectionCSR';
+import { useState, useEffect } from 'react';
 
 export default function AddCarPage() {
-  const isMobile = isMobileCSR();
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth <= 680);
+  }, []);
 
   return (
-    <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white pt-24 pb-12">
+    <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white md:pt-24 pb-12 relative">
       {isMobile ? <MobileAddCar /> : <AddCar />}
     </main>
   );
 }
+//ovo sam ubacio jer je server i browser su renderovali razlicite komponete pa je next pucao tj. izbacivao je hydration error
